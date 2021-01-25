63599b304ca1296ae67c2dcbb20ce3c5a5fd8781
Marvin Maitland <marvin.maitland@gmail.com>
2021-01-24 20:16:11 -0500

    public folder and index.html add

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.defaults('db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.get('/public/index', require('./public/index'));
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on ${port}`);
+});
